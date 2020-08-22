20bccea572ea90f4579629e660ce9593da6bda13
Hamza-ye <7amza.it@gmail.com>
2020-08-22 23:38:02 +0300

    Initial commit

---
+++ abstract-adapter/src/main/java/com/mass3d/abstractadapter/AbstractAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.mass3d.abstractadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbstractAdapterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AbstractAdapterApplication.class, args);
+  }
+
+}
---
+++ abstract-adapter/src/test/java/com/mass3d/abstractadapter/AbstractAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mass3d.abstractadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbstractAdapterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
